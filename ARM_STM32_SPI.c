9e9289bc38b2ed6dfb93b767eb5c4ca2136def4f
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2017-03-22 16:18:21 +0530

    added basic initialization and parameter setting functions

--- ARM_STM32_SPI.c
+++ ARM_STM32_SPI.c
@@ -16,27 +16,30 @@
 #include "ARM_STM32_SPI.h"
 
 
-void SPI_Init_Pins(uint8_t spi_num, uint8_t spi_gpio_speed, GPIO_TypeDef* spi_cs_port, uint16_t spi_cs_pin_num)
+void ARM_STM32_SPI_Init_Pins(uint8_t spi_num, uint8_t spi_gpio_speed, GPIO_TypeDef* spi_cs_port, uint16_t spi_cs_pin_num)
 {
 	//INTIALIZE SPI GPIO PINS DEPENDING ON WHICH SPI PERIPHERAL IS SPECIFIED
 	
+	#ifdef STM32F103
 	GPIO_InitTypeDef gpio;
-	if(spi_num == SPI_PERIPHERAL_SPI1)
+	if(spi_num == ARM_STM32_SPI_PERIPHERAL_SPI1)
 	{
+		//SPI1 PERIPHERAL
 		gpio.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
 		gpio.GPIO_Mode = GPIO_Mode_AF_PP;
 		gpio.GPIO_Speed = (GPIOSpeed_TypeDef)spi_gpio_speed;
 		GPIO_Init(GPIOA, &gpio);
 	}
-	else if(spi_num == SPI_PERIPHERAL_SPI2)
+	else if(spi_num == ARM_STM32_SPI_PERIPHERAL_SPI2)
 	{
+		//SPI2 PERIPHERAL
 		gpio.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
 		gpio.GPIO_Mode = GPIO_Mode_AF_PP;
 		gpio.GPIO_Speed = (GPIOSpeed_TypeDef)spi_gpio_speed;
 		GPIO_Init(GPIOB, &gpio);
 	}
 	
-	//SET CS PIN
+	//SET CS PIN TO PUT PUSH PULL
 	gpio.GPIO_Mode = GPIO_Mode_Out_PP;
 	gpio.GPIO_Speed = (GPIOSpeed_TypeDef)spi_gpio_speed;
 	gpio.GPIO_Pin = spi_cs_pin_num;
@@ -44,20 +47,109 @@ void SPI_Init_Pins(uint8_t spi_num, uint8_t spi_gpio_speed, GPIO_TypeDef* spi_cs
 	
 	//SET CS TO HIGH
 	GPIO_SetBits(spi_cs_port, spi_cs_pin_num);
+	#endif
 }
 
-void SPI_Set_Parameters(uint8_t spi_num)
+void ARM_STM32_SPI_Set_Parameters(uint8_t spi_num, uint16_t spi_datasize, uint16_t spi_speed, uint16_t spi_direction, uint16_t spi_msb_lsb_first)
 {
 	//SET THE PARAMETERS OF THE SPECIFIED SPI
 	
-	if(spi_num == SPI_PERIPHERAL_SPI1)
+	#ifdef STM32F103
+	if(spi_num == ARM_STM32_SPI_PERIPHERAL_SPI1)
 	{
+		//SPI1 PERIPHERAL
 		
+		//ENABLE CLOCK TO SPI1
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+		
+		//CONFIGURE SPI1 PARAMETERS
+		SPI_InitTypeDef spi;
+		
+		spi.SPI_DataSize = spi_datasize;
+		spi.SPI_BaudRatePrescaler= spi_speed;
+		spi.SPI_Direction = spi_direction;
+		spi.SPI_FirstBit = spi_msb_lsb_first;
+		spi.SPI_CPHA = SPI_CPHA_1Edge;
+		spi.SPI_CPOL = SPI_CPOL_Low;
+		spi.SPI_Mode = SPI_Mode_Master;
+		spi.SPI_NSS = SPI_NSS_Soft;
+		
+		SPI_Init(SPI1, &spi);
 	}
-	else if(spi_num == SPI_PERIPHERAL_SPI2)
+	else if(spi_num == ARM_STM32_SPI_PERIPHERAL_SPI2)
 	{
+		//SPI2 PERIPHERAL
 		
+		//ENABLE CLOCK TO SPI1
+		RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
+		
+		SPI_InitTypeDef spi;
+		
+		spi.SPI_DataSize = spi_datasize;
+		spi.SPI_BaudRatePrescaler= spi_speed;
+		spi.SPI_Direction = spi_direction;
+		spi.SPI_FirstBit = spi_msb_lsb_first;
+		spi.SPI_CPHA = SPI_CPHA_1Edge;
+		spi.SPI_CPOL = SPI_CPOL_Low;
+		spi.SPI_Mode = SPI_Mode_Master;
+		spi.SPI_NSS = SPI_NSS_Soft;
+		
+		SPI_Init(SPI2, &spi);
 	}
+	#endif
 }
 
-void SPI_Send_Data(void);
+void ARM_STM32_SPI_Start(uint8_t spi_num)
+{
+	//START SPECIDIED SPI PERIPHERAL
+	
+	#ifdef STM32F103
+	if(spi_num == ARM_STM32_SPI_PERIPHERAL_SPI1)
+	{
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+		SPI_Cmd(SPI1, ENABLE);
+	}
+	else if (spi_num == ARM_STM32_SPI_PERIPHERAL_SPI2)
+	{
+		RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
+		SPI_Cmd(SPI1, ENABLE);
+	}
+	#endif
+}
+
+void ARM_STM32_SPI_Stop(uint8_t spi_num)
+{
+	//DISABLE THE SPECIFIED SPI AND STOP CLOCK TO IT
+	
+	#ifdef STM32F103
+	if(spi_num == ARM_STM32_SPI_PERIPHERAL_SPI1)
+	{
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, DISABLE);
+		SPI_Cmd(SPI1, DISABLE);
+	}
+	else if (spi_num == ARM_STM32_SPI_PERIPHERAL_SPI2)
+	{
+		RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, DISABLE);
+		SPI_Cmd(SPI2, DISABLE);
+	}
+	#endif
+}
+void ARM_STM32_SPI_Send_Data(void);
+
+void ARM_STM32_SPI_Deinit(uint8_t spi_num)
+{
+	//DEINTIALIZE THE SPI PERPHERAL
+	
+	#ifdef STM32F103
+	if(spi_num == ARM_STM32_SPI_PERIPHERAL_SPI1)
+	{
+		//SPI1 PERIPHERAL
+		SPI_I2S_DeInit(SPI1);
+	}
+	else if (spi_num == ARM_STM32_SPI_PERIPHERAL_SPI2)
+	{
+		//SPI2 PERIPHERAL
+		SPI_I2S_DeInit(SPI2);
+	}
+	#endif
+}
--- ARM_STM32_SPI.h
+++ ARM_STM32_SPI.h
@@ -20,25 +20,49 @@
 //THE RIGHT FILE FROM STM SPL
 //THE MCU SHOULD BE DEFINED IN TARGET OPTIONS -> C/C++
 #ifdef STM32F103
+	#include "stm32f10x.h"
 	#include "stm32f10x_spi.h"
 	#include "stm32f10x_gpio.h"
 	
-	#define SPI_PERIPHERAL_SPI1	1
-	#define SPI_PERIPHERAL_SPI2	2
+	#define ARM_STM32_SPI_PERIPHERAL_SPI1	1
+	#define ARM_STM32_SPI_PERIPHERAL_SPI2	2
 	
-	#define SPI_GPIO_SPEED_2MHZ 	2
-	#define SPI_GPIO_SPEED_10MHZ 	1
-	#define SPI_GPIO_SPEED_50MHZ	3
+	#define ARM_STM32_SPI_GPIO_SPEED_2MHZ 	2
+	#define ARM_STM32_SPI_GPIO_SPEED_10MHZ 	1
+	#define ARM_STM32_SPI_GPIO_SPEED_50MHZ	3
 	
-	#define SPI_CS_HARD
+	#define ARM_STM32_SPI_DATASIZE_16	SPI_DataSize_16b
+	#define ARM_STM32_SPI_DATASIZE_8 	SPI_DataSize_8b
+	
+	#define ARM_STM32_SPI_BAUD_PRESCALER_2		SPI_BaudRatePrescaler_2
+	#define ARM_STM32_SPI_BAUD_PRESCALER_4 		SPI_BaudRatePrescaler_4
+	#define ARM_STM32_SPI_BAUD_PRESCALER_8 		SPI_BaudRatePrescaler_8
+	#define ARM_STM32_SPI_BAUD_PRESCALER_16 	SPI_BaudRatePrescaler_16
+	#define ARM_STM32_SPI_BAUD_PRESCALER_32 	SPI_BaudRatePrescaler_32
+	#define ARM_STM32_SPI_BAUD_PRESCALER_64 	SPI_BaudRatePrescaler_64
+	#define ARM_STM32_SPI_BAUD_PRESCALER_128	SPI_BaudRatePrescaler_128
+	#define ARM_STM32_SPI_BAUD_PRESCALER_256	SPI_BaudRatePrescaler_256
+
+	#define ARM_STM32_SPI_MSB_FIRST	SPI_FirstBit_MSB
+	#define ARM_STM32_SPI_LSB_FIRST	SPI_FirstBit_LSB
+	
+	#define ARM_STM32_SPI_DIRECTION_2LINE_FULLDUPLEX	SPI_Direction_2Lines_FullDuplex
+	#define ARM_STM32_SPI_DIRECTION_2LINE_RX_ONLY			SPI_Direction_2Lines_RxOnly
+	#define ARM_STM32_SPI_DIRECTION_1LINE_RX					SPI_Direction_1Line_Rx 
+	#define ARM_STM32_SPI_DIRECTION_1LINE_TX					SPI_Direction_1Line_Tx       
 #endif
 
 #ifdef STM32F072
 #endif
 
 
-void SPI_Init_Pins(uint8_t spi_num, uint8_t spi_gpio_speed, GPIO_TypeDef* spi_cs_port, uint16_t spi_cs_pin_num);
-void SPI_Set_Parameters(uint8_t spi_num);
-void SPI_Send_Data(void);
+void ARM_STM32_SPI_Init_Pins(uint8_t spi_num, uint8_t spi_gpio_speed, GPIO_TypeDef* spi_cs_port, uint16_t spi_cs_pin_num);
+void ARM_STM32_SPI_Set_Parameters(uint8_t spi_num, uint16_t spi_datasize, uint16_t spi_speed, uint16_t spi_direction, uint16_t spi_msb_lsb_first);
+void ARM_STM32_SPI_Start(uint8_t spi_num);
+void ARM_STM32_SPI_Stop(uint8_t spi_num);
+
+void ARM_STM32_SPI_Send_Data(void);
+
+void ARM_STM32_SPI_Deinit(uint8_t spi_num);
 
 #endif
