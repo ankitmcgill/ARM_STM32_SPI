2b9a53d6c4a378379ebed03cd278f331c71945df
Ankit Bhatnagar <ankit.bhatnagarindia@gmail.com>
2017-03-22 15:00:51 +0530

    added spi gpio initialization function

--- ARM_STM32_SPI.c
+++ ARM_STM32_SPI.c
@@ -1,12 +1,63 @@
-/////////////////////////////////////////////////////
-// ARM - STM32 - SPI LIBRARY
-//
-// MARCH 22 2017
-//
-// ANKIT BHATNAGAR
-// ANKIT.BHATNAGARINDIA@GMAIL.COM
-/////////////////////////////////////////////////////
+/*************************************************************************
+* STM32 SPI LIBRARY (SPL WRAPPER)
+*
+* SUPPORTS ONLY SPI MASTER MODE
+* SUPPORTS CS SOFTWARE MODE ONLY (NO HARDWARE SUPPORT)
+*	SUPPORTS ONLY DEFAULT SPI PIN (NO PIN REMAPPING)
+*
+* MARCH 22 2016
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+*
+* REFERENCES
+* ***********************************************************************/
+
 
 #include "ARM_STM32_SPI.h"
 
 
+void SPI_Init_Pins(uint8_t spi_num, uint8_t spi_gpio_speed, GPIO_TypeDef* spi_cs_port, uint16_t spi_cs_pin_num)
+{
+	//INTIALIZE SPI GPIO PINS DEPENDING ON WHICH SPI PERIPHERAL IS SPECIFIED
+	
+	GPIO_InitTypeDef gpio;
+	if(spi_num == SPI_PERIPHERAL_SPI1)
+	{
+		gpio.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;
+		gpio.GPIO_Mode = GPIO_Mode_AF_PP;
+		gpio.GPIO_Speed = (GPIOSpeed_TypeDef)spi_gpio_speed;
+		GPIO_Init(GPIOA, &gpio);
+	}
+	else if(spi_num == SPI_PERIPHERAL_SPI2)
+	{
+		gpio.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+		gpio.GPIO_Mode = GPIO_Mode_AF_PP;
+		gpio.GPIO_Speed = (GPIOSpeed_TypeDef)spi_gpio_speed;
+		GPIO_Init(GPIOB, &gpio);
+	}
+	
+	//SET CS PIN
+	gpio.GPIO_Mode = GPIO_Mode_Out_PP;
+	gpio.GPIO_Speed = (GPIOSpeed_TypeDef)spi_gpio_speed;
+	gpio.GPIO_Pin = spi_cs_pin_num;
+	GPIO_Init(spi_cs_port, &gpio);
+	
+	//SET CS TO HIGH
+	GPIO_SetBits(spi_cs_port, spi_cs_pin_num);
+}
+
+void SPI_Set_Parameters(uint8_t spi_num)
+{
+	//SET THE PARAMETERS OF THE SPECIFIED SPI
+	
+	if(spi_num == SPI_PERIPHERAL_SPI1)
+	{
+		
+	}
+	else if(spi_num == SPI_PERIPHERAL_SPI2)
+	{
+		
+	}
+}
+
+void SPI_Send_Data(void);
--- ARM_STM32_SPI.h
+++ ARM_STM32_SPI.h
@@ -1,15 +1,44 @@
-/////////////////////////////////////////////////////
-// ARM - STM32 - SPI LIBRARY
-//
-// MARCH 22 2017
-//
-// ANKIT BHATNAGAR
-// ANKIT.BHATNAGARINDIA@GMAIL.COM
-/////////////////////////////////////////////////////
+/*************************************************************************
+* STM32 SPI LIBRARY (SPL WRAPPER)
+*
+* SUPPORTS ONLY SPI MASTER MODE
+* SUPPORTS CS SOFTWARE MODE ONLY (NO HARDWARE SUPPORT)
+*	SUPPORTS ONLY DEFAULT SPI PIN (NO PIN REMAPPING)
+*
+* MARCH 22 2016
+* ANKIT BHATNAGAR
+* ANKIT.BHATNAGARINDIA@GMAIL.COM
+*
+* REFERENCES
+* ***********************************************************************/
 
 #ifndef _ARM_STM32_SPI_
 #define _ARM_STM32_SPI_
 
-#include<stdint.h>
+#include <stdint.h>
+//CHECK WHAT MCU US BEING USED IN ORDER TO INCLUDE
+//THE RIGHT FILE FROM STM SPL
+//THE MCU SHOULD BE DEFINED IN TARGET OPTIONS -> C/C++
+#ifdef STM32F103
+	#include "stm32f10x_spi.h"
+	#include "stm32f10x_gpio.h"
+	
+	#define SPI_PERIPHERAL_SPI1	1
+	#define SPI_PERIPHERAL_SPI2	2
+	
+	#define SPI_GPIO_SPEED_2MHZ 	2
+	#define SPI_GPIO_SPEED_10MHZ 	1
+	#define SPI_GPIO_SPEED_50MHZ	3
+	
+	#define SPI_CS_HARD
+#endif
+
+#ifdef STM32F072
+#endif
+
+
+void SPI_Init_Pins(uint8_t spi_num, uint8_t spi_gpio_speed, GPIO_TypeDef* spi_cs_port, uint16_t spi_cs_pin_num);
+void SPI_Set_Parameters(uint8_t spi_num);
+void SPI_Send_Data(void);
 
 #endif
